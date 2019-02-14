acc4789ba4e559fb7e6d13e523a961665f3d8c0b
Jason <jason.calderon@sascholar.org>
2019-02-14 20:54:24 +0000

    i did it

--- Deliverable:gif-finder/script.js
+++ Deliverable:gif-finder/script.js
@@ -3,7 +3,14 @@
 //Beneath this comment: Query Select your input by grabbing th id of the input element from index.html
 
 //Beneath this comment: Code out a click event listener for your button that logs "Button pressed!" to the console when it is clicked 
-
+let mainbutton = document.querySelector("#mainButton")
+let input = document.querySelector("#input")
+mainbutton.addEventListener("click" , e => {
+  console.log("buttonpressed")
+  let searchInput = input.value
+  console.log(searchInput)
+  sendApiRequest(searchInput)
+})
 
 //Now, inside your event listener create a new variable called searchInput and call the .value method on the input variable you created. 
 
@@ -12,13 +19,17 @@
 // Go to https://developers.giphy.com/ and create an account. Then create your first app called testApp to create an API key. Enter it below where it says: YOUR_API_KEY_GOES_HERE.
 function sendApiRequest(x) {
   //Where Ryan+Gosling is located replace it with x
-    fetch("https://api.giphy.com/v1/gifs/search?api_key=YOUR_API_KEY_GOES_HERE&q=Ryan+Gosling&limit=25&offset=0&rating=G&lang=en")
+    fetch("https://api.giphy.com/v1/gifs/search?api_key=dJertxev3XKxnByVqSNpNQi4f1D3VIGD&q="+x+"&limit=25&offset=0&rating=G&lang=en")
     .then(function(data) {
       return data.json();
     })
     .then(function(json) {
       console.log(json);
-      //Beneath this line: pass the JSON on to the next function. HINT: Call getImageURLfrom() and pass in json as your argument.
+      //Beneath this line: pass the JSON on to the next function. HINT: Call  and pass in json as your argument.
+  getImageURLfrom(json)
+
+
+
 
     });
 };
@@ -26,23 +37,21 @@ function sendApiRequest(x) {
 // Get a specific image URL ending in .gif from your JSON search results. Pass it on to the next function.
 function getImageURLfrom(myJSON) {
   //create  a const called imageURL and set it to equal myJSON.data[0].images.original.url 
-
+const imageURL =  myJSON.data[Math.floor(Math.random() * 25)].images.original.url 
   //console log your imageURL const
-  
+  console.log(imageURL)
   //Now call your addImageToScreen function below and pass in your const as it's argument
-  
+  addImageToScreen(imageURL)
   
 };
 
   //querySelect the wrapper, and add an image tag to it. Interpolate the URL string from the previous function.
 function addImageToScreen(myURL) {
   //Now create a variable called wrapper and querySelect the div with an id of wrapper
-  
+  let hope = document.querySelector("#wrapper")
   //Next call the .innerHTML method on your wrapper varible and set it equal to the following string "<img src = ''>"
   //Where the single quotes are located concactenate your myURL parameter to the img tag element.
-  
+  hope.innerHTML =  "<img src = '"+myURL+"'>"
 };
 
 
-//
-// 
